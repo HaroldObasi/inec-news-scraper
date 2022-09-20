981457b430e2ac4c4c63311465ef4ec74c564a21
HaroldObasi <haroldobasi2k16@gmail.com>
2022-09-20 19:51:50 +0100

    used By object, used service object driver

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from emailer import send_emails
 import os
 
@@ -8,7 +11,11 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+service_obj = Service(location)
+driver = webdriver.Chrome(service=service_obj)
+
 
 sender = "haroldobasi2k16@gmail.com"
 recievers = ["haroldobasibackup@gmail.com"]
@@ -18,18 +25,19 @@ inec_news = "https://inecnews.com/"
 # driver = webdriver.Chrome(location)
 
 driver.get(inec_news)
+driver.implicitly_wait(20)
 
-news_dates = driver.find_elements_by_css_selector(".two-third .post-head .updated")
-news_titles = driver.find_elements_by_css_selector(".two-third .post-title")
+news_titles = driver.find_elements(By.CSS_SELECTOR, ".two-third .post-title")
+news_dates = driver.find_elements(By.CSS_SELECTOR,".two-third .post-head .updated")
 
 news = {}
 
 for i in range(len(news_dates)):
   news[i] = {
-    "title": news_titles[i].text,
-    "date": news_dates[i].text
+    "title": news_titles[i].get_attribute("textContent"),
+    "date": news_dates[i].get_attribute("textContent")
   }
 
-send_emails("haroldobasi2k19@gmail.com", {"subject": "Testing Final", "body": "Placeholder"}, news)
-print("news: ", news)
-driver.quit()
+print(news)
+
+driver.quit()#Content > div > div.sections_group > div > div:nth-child(3) > div > div.wrap.mcb-wrap.two-third.valign-top.clearfix > div > div > div > div > div.posts_group.lm_wrapper.col-2.photo > div.post-item.isotope-item.clearfix.author-rotimi.post-5033.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-uncategorized > div.post-desc-wrapper > div > 
